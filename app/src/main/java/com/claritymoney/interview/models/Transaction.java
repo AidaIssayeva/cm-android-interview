56abe9ce9bba1f7d2507edf4455800c1a83f0bd4
Fahim <fahimkarim@gmail.com>
2017-08-17 14:00:51 -0700

    initial commit

---
+++ app/src/main/java/com/claritymoney/interview/models/Transaction.java
@@ -0,0 +1,14 @@
+package com.claritymoney.interview.models;
+
+/**
+ * Created by fahim on 8/17/17.
+ */
+
+public class Transaction {
+    public String category = "Shopping";
+    public String institutionName = "Amazon";
+    public String accountName = "Amazon - 8586";
+    public String date = "August 06, 2017";
+    public double amount = 15.32;
+    public String logoUrl = "https://static.claritymoney.com/images/logos/brands/amazon.jpg";
+}
